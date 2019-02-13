9225483bc9ddaa734f241de2fe5a4ebf9a7b9d5e
Andrey Bobrovskiy <A.Bobrovskiy@gmail.com>
2019-02-13 18:33:41 +0300

    animation of run

--- animation.py
+++ animation.py
@@ -20,7 +20,6 @@ pygame.init()
 
 class AnimatedSprite(pygame.sprite.Sprite):
     def __init__(self, sheet, columns, rows, x, y, k1=-1, k2=-1):
-        super().__init__(all_sprites)
         self.frames = []
         self.cut_sheet(sheet, columns, rows)
         self.cur_frame = 0
@@ -63,7 +62,8 @@ pygame.draw.rect(screen, pygame.Color('white'), (0, 0, width, height), 0)
 
 pygame.display.flip()
 image = pygame.Surface([100, 100])
-
+bear = AnimatedSprite(load_image("Bear.png"), 8, 8, 50, 50)
+screen.blit(bear.image, bear.rect)
 bear_run = AnimatedSprite(load_image("Bear.png"), 8, 8, 50, 50, 10, 22)
 
 bear_hit = AnimatedSprite(load_image("Bear.png"), 8, 8, 50, 50, 23, 31)
@@ -76,6 +76,7 @@ all_sprites.draw(screen)
 pygame.display.update()
 running = True
 evtype = 0
+x = 50
 evkey = 0
 while running:
     for event in pygame.event.get():
@@ -84,28 +85,42 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 for i in range(51 - 42):
-                    bear_jump.update()
                     pygame.draw.rect(screen, pygame.Color('white'), (0, 0, width, height), 0)
-                    all_sprites.draw(screen)
-                    clock.tick(10)
+                    screen.blit(bear_jump.image, bear_jump.rect)
+                    bear_jump.update()
+                    clock.tick(15)
                     pygame.display.update()
+                pygame.draw.rect(screen, pygame.Color('white'), (0, 0, width, height), 0)
+                screen.blit(bear.image, bear.rect)
+                pygame.display.update()
+
             if event.key == pygame.K_q:
                 for i in range(31 - 23):
-                    bear_hit.update()
                     pygame.draw.rect(screen, pygame.Color('white'), (0, 0, width, height), 0)
-                    all_sprites.draw(screen)
-                    clock.tick(10)
+                    screen.blit(bear_hit.image, bear_hit.rect)
+                    bear_hit.update()
+                    clock.tick(15)
                     pygame.display.update()
+                pygame.draw.rect(screen, pygame.Color('white'), (0, 0, width, height), 0)
+                screen.blit(bear.image, bear.rect)
+                pygame.display.update()
             evtype = event.type
             evkey = event.key
         if event.type == pygame.KEYUP and (event.key == pygame.K_RIGHT):
             evtype = 0
             evkey = 0
     while evtype == pygame.KEYDOWN and evkey == pygame.K_RIGHT:
-        bear_run.update()
-        pygame.draw.rect(screen, pygame.Color('white'), (0, 0, width, height), 0)
-        all_sprites.draw(screen)
-        clock.tick(10)
-        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYUP and (event.key == pygame.K_RIGHT):
+                evtype = 0
+                evkey = 0
+
+            for i in range(22 - 10):
+                bear_run = AnimatedSprite(load_image("Bear.png"), 8, 8, x, 50, 10 + i, 22)
+                x += 1
+                pygame.draw.rect(screen, pygame.Color('white'), (0, 0, width, height), 0)
+                screen.blit(bear_run.image, bear_run.rect)
+                clock.tick(10)
+                pygame.display.update()
 
 pygame.quit()
